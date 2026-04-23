e1c74d42c7d3360973ee0c4145814ebacf595fb7
Matyas646 <72499070+Matyas646@users.noreply.github.com>
2026-04-24 00:25:42 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@
 chip_size = 50
 
 # Pygame graphics (Needed for human players)
-graphics = False
+graphics = True
 
 # Moves per second (zero for off, can't be higher than fps)
 # Should only be used in bot vs bot
@@ -12,8 +12,8 @@ fps = 60
 summation = 1 # How often the wins get summed
 
 # Who plays as White and Black
-B_Player = "botond"
-W_Player = "botond"
+B_Player = "hard_bot"
+W_Player = "human"
 
 Height = 14.5*chip_size
 Width = 14.5*chip_size
